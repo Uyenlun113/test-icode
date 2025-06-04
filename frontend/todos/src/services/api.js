0167d5847092b077895b02ed32ef290ab073a6f6
Uyenlun113 <thiuyen1132004@gmail.com>
2025-06-04 15:37:04 +0700

    update env

--- frontend/todos/src/services/api.js
+++ frontend/todos/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 // Cấu hình base URL
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-// Tạo axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
